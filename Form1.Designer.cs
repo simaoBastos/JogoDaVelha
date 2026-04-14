6aaa43579f9ba8f94a9eb4915c6d694891107283
wcc_devs <wcc_devs@WCC-LAB36-61325>
2026-04-13 21:31:30 -0300

    Redesign UI and add full game logic

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -37,18 +37,24 @@
             btn9 = new Button();
             btn8 = new Button();
             btn7 = new Button();
-            button1 = new Button();
+            btnReiniciar = new Button();
             panel1 = new Panel();
             panel2 = new Panel();
             panel3 = new Panel();
             panel4 = new Panel();
-            label1 = new Label();
+            lblVez = new Label();
             pnlTelaInicial = new Panel();
+            pnlTelaFinal = new Panel();
             btnSair = new Button();
             btnJogar = new Button();
             lblTitulo = new Label();
             fontDialog1 = new FontDialog();
+            lblTituloFinal = new Label();
+            lblReceba = new Label();
+            button1 = new Button();
+            button2 = new Button();
             pnlTelaInicial.SuspendLayout();
+            pnlTelaFinal.SuspendLayout();
             SuspendLayout();
             // 
             // btn1
@@ -60,10 +66,11 @@
             btn1.FlatAppearance.MouseOverBackColor = Color.Transparent;
             btn1.FlatStyle = FlatStyle.Flat;
             btn1.Font = new Font("MV Boli", 40F, FontStyle.Bold);
-            btn1.ForeColor = Color.White;
-            btn1.Location = new Point(10, 37);
+            btn1.ForeColor = Color.WhiteSmoke;
+            btn1.Location = new Point(17, 62);
+            btn1.Margin = new Padding(5);
             btn1.Name = "btn1";
-            btn1.Size = new Size(80, 80);
+            btn1.Size = new Size(133, 135);
             btn1.TabIndex = 0;
             btn1.TabStop = false;
             btn1.UseVisualStyleBackColor = false;
@@ -78,10 +85,11 @@
             btn2.FlatAppearance.MouseOverBackColor = Color.Transparent;
             btn2.FlatStyle = FlatStyle.Flat;
             btn2.Font = new Font("MV Boli", 40F, FontStyle.Bold);
-            btn2.ForeColor = Color.White;
-            btn2.Location = new Point(96, 37);
+            btn2.ForeColor = Color.WhiteSmoke;
+            btn2.Location = new Point(160, 62);
+            btn2.Margin = new Padding(5);
             btn2.Name = "btn2";
-            btn2.Size = new Size(80, 80);
+            btn2.Size = new Size(133, 135);
             btn2.TabIndex = 1;
             btn2.TabStop = false;
             btn2.UseVisualStyleBackColor = false;
@@ -96,10 +104,11 @@
             btn3.FlatAppearance.MouseOverBackColor = Color.Transparent;
             btn3.FlatStyle = FlatStyle.Flat;
             btn3.Font = new Font("MV Boli", 40F, FontStyle.Bold);
-            btn3.ForeColor = Color.White;
-            btn3.Location = new Point(182, 37);
+            btn3.ForeColor = Color.WhiteSmoke;
+            btn3.Location = new Point(303, 62);
+            btn3.Margin = new Padding(5);
             btn3.Name = "btn3";
-            btn3.Size = new Size(80, 80);
+            btn3.Size = new Size(133, 135);
             btn3.TabIndex = 2;
             btn3.TabStop = false;
             btn3.UseVisualStyleBackColor = false;
@@ -114,10 +123,11 @@
             btn6.FlatAppearance.MouseOverBackColor = Color.Transparent;
             btn6.FlatStyle = FlatStyle.Flat;
             btn6.Font = new Font("MV Boli", 40F, FontStyle.Bold);
-            btn6.ForeColor = Color.White;
-            btn6.Location = new Point(182, 123);
+            btn6.ForeColor = Color.WhiteSmoke;
+            btn6.Location = new Point(303, 207);
+            btn6.Margin = new Padding(5);
             btn6.Name = "btn6";
-            btn6.Size = new Size(80, 80);
+            btn6.Size = new Size(133, 135);
             btn6.TabIndex = 5;
             btn6.TabStop = false;
             btn6.UseVisualStyleBackColor = false;
@@ -132,10 +142,11 @@
             btn5.FlatAppearance.MouseOverBackColor = Color.Transparent;
             btn5.FlatStyle = FlatStyle.Flat;
             btn5.Font = new Font("MV Boli", 40F, FontStyle.Bold);
-            btn5.ForeColor = Color.White;
-            btn5.Location = new Point(96, 123);
+            btn5.ForeColor = Color.WhiteSmoke;
+            btn5.Location = new Point(160, 207);
+            btn5.Margin = new Padding(5);
             btn5.Name = "btn5";
-            btn5.Size = new Size(80, 80);
+            btn5.Size = new Size(133, 135);
             btn5.TabIndex = 4;
             btn5.TabStop = false;
             btn5.UseVisualStyleBackColor = false;
@@ -150,10 +161,11 @@
             btn4.FlatAppearance.MouseOverBackColor = Color.Transparent;
             btn4.FlatStyle = FlatStyle.Flat;
             btn4.Font = new Font("MV Boli", 40F, FontStyle.Bold);
-            btn4.ForeColor = Color.White;
-            btn4.Location = new Point(10, 123);
+            btn4.ForeColor = Color.WhiteSmoke;
+            btn4.Location = new Point(17, 207);
+            btn4.Margin = new Padding(5);
             btn4.Name = "btn4";
-            btn4.Size = new Size(80, 80);
+            btn4.Size = new Size(133, 135);
             btn4.TabIndex = 3;
             btn4.TabStop = false;
             btn4.UseVisualStyleBackColor = false;
@@ -168,10 +180,11 @@
             btn9.FlatAppearance.MouseOverBackColor = Color.Transparent;
             btn9.FlatStyle = FlatStyle.Flat;
             btn9.Font = new Font("MV Boli", 40F, FontStyle.Bold);
-            btn9.ForeColor = Color.White;
-            btn9.Location = new Point(182, 209);
+            btn9.ForeColor = Color.WhiteSmoke;
+            btn9.Location = new Point(303, 352);
+            btn9.Margin = new Padding(5);
             btn9.Name = "btn9";
-            btn9.Size = new Size(80, 80);
+            btn9.Size = new Size(133, 135);
             btn9.TabIndex = 8;
             btn9.TabStop = false;
             btn9.UseVisualStyleBackColor = false;
@@ -186,10 +199,11 @@
             btn8.FlatAppearance.MouseOverBackColor = Color.Transparent;
             btn8.FlatStyle = FlatStyle.Flat;
             btn8.Font = new Font("MV Boli", 40F, FontStyle.Bold);
-            btn8.ForeColor = Color.White;
-            btn8.Location = new Point(96, 209);
+            btn8.ForeColor = Color.WhiteSmoke;
+            btn8.Location = new Point(160, 352);
+            btn8.Margin = new Padding(5);
             btn8.Name = "btn8";
-            btn8.Size = new Size(80, 80);
+            btn8.Size = new Size(133, 135);
             btn8.TabIndex = 7;
             btn8.TabStop = false;
             btn8.UseVisualStyleBackColor = false;
@@ -204,100 +218,126 @@
             btn7.FlatAppearance.MouseOverBackColor = Color.Transparent;
             btn7.FlatStyle = FlatStyle.Flat;
             btn7.Font = new Font("MV Boli", 40F, FontStyle.Bold);
-            btn7.ForeColor = Color.White;
-            btn7.Location = new Point(10, 209);
+            btn7.ForeColor = Color.WhiteSmoke;
+            btn7.Location = new Point(17, 352);
+            btn7.Margin = new Padding(5);
             btn7.Name = "btn7";
-            btn7.Size = new Size(80, 80);
+            btn7.Size = new Size(133, 135);
             btn7.TabIndex = 6;
             btn7.TabStop = false;
             btn7.UseVisualStyleBackColor = false;
             btn7.Click += BotaoGrid_Click;
             // 
-            // button1
+            // btnReiniciar
             // 
-            button1.BackColor = Color.SteelBlue;
-            button1.FlatAppearance.BorderSize = 0;
-            button1.FlatStyle = FlatStyle.Flat;
-            button1.Font = new Font("MV Boli", 12F, FontStyle.Bold);
-            button1.ForeColor = Color.White;
-            button1.Location = new Point(8, 301);
-            button1.Name = "button1";
-            button1.Size = new Size(256, 37);
-            button1.TabIndex = 9;
-            button1.TabStop = false;
-            button1.Text = "REINICIAR JOGO";
-            button1.UseVisualStyleBackColor = false;
+            btnReiniciar.BackColor = Color.DimGray;
+            btnReiniciar.FlatAppearance.BorderSize = 0;
+            btnReiniciar.FlatStyle = FlatStyle.Flat;
+            btnReiniciar.Font = new Font("MV Boli", 12F, FontStyle.Bold);
+            btnReiniciar.ForeColor = Color.WhiteSmoke;
+            btnReiniciar.Location = new Point(13, 506);
+            btnReiniciar.Margin = new Padding(5);
+            btnReiniciar.Name = "btnReiniciar";
+            btnReiniciar.Size = new Size(427, 62);
+            btnReiniciar.TabIndex = 9;
+            btnReiniciar.TabStop = false;
+            btnReiniciar.Text = "REINICIAR JOGO";
+            btnReiniciar.UseVisualStyleBackColor = false;
+            btnReiniciar.Click += btnReiniciar_Click;
             // 
             // panel1
             // 
-            panel1.BackColor = Color.FromArgb(60, 60, 60);
+            panel1.BackColor = Color.DarkGray;
             panel1.Enabled = false;
-            panel1.Location = new Point(8, 119);
+            panel1.Location = new Point(13, 200);
+            panel1.Margin = new Padding(5);
             panel1.Name = "panel1";
-            panel1.Size = new Size(256, 2);
+            panel1.Size = new Size(427, 3);
             panel1.TabIndex = 10;
             // 
             // panel2
             // 
-            panel2.BackColor = Color.FromArgb(60, 60, 60);
+            panel2.BackColor = Color.DarkGray;
             panel2.Enabled = false;
-            panel2.Location = new Point(8, 205);
+            panel2.Location = new Point(13, 345);
+            panel2.Margin = new Padding(5);
             panel2.Name = "panel2";
-            panel2.Size = new Size(256, 2);
+            panel2.Size = new Size(427, 3);
             panel2.TabIndex = 11;
             // 
             // panel3
             // 
-            panel3.BackColor = Color.FromArgb(60, 60, 60);
+            panel3.BackColor = Color.DarkGray;
             panel3.Enabled = false;
-            panel3.Location = new Point(92, 35);
+            panel3.Location = new Point(153, 59);
+            panel3.Margin = new Padding(5);
             panel3.Name = "panel3";
-            panel3.Size = new Size(2, 256);
+            panel3.Size = new Size(3, 431);
             panel3.TabIndex = 11;
             // 
             // panel4
             // 
-            panel4.BackColor = Color.FromArgb(60, 60, 60);
+            panel4.BackColor = Color.DarkGray;
             panel4.Enabled = false;
-            panel4.Location = new Point(178, 35);
+            panel4.Location = new Point(297, 59);
+            panel4.Margin = new Padding(5);
             panel4.Name = "panel4";
-            panel4.Size = new Size(2, 256);
+            panel4.Size = new Size(3, 431);
             panel4.TabIndex = 12;
             // 
-            // label1
+            // lblVez
             // 
-            label1.BackColor = Color.Transparent;
-            label1.Font = new Font("MV Boli", 15F, FontStyle.Bold);
-            label1.ForeColor = Color.White;
-            label1.Location = new Point(8, 3);
-            label1.Name = "label1";
-            label1.Size = new Size(256, 31);
-            label1.TabIndex = 13;
-            label1.Text = "Vez do Jogador: X";
-            label1.TextAlign = ContentAlignment.MiddleCenter;
-            label1.UseCompatibleTextRendering = true;
+            lblVez.BackColor = Color.Transparent;
+            lblVez.Font = new Font("MV Boli", 15F, FontStyle.Bold);
+            lblVez.ForeColor = Color.WhiteSmoke;
+            lblVez.Location = new Point(13, 5);
+            lblVez.Margin = new Padding(5, 0, 5, 0);
+            lblVez.Name = "lblVez";
+            lblVez.Size = new Size(427, 52);
+            lblVez.TabIndex = 13;
+            lblVez.Text = "Vez do jogador: X";
+            lblVez.TextAlign = ContentAlignment.MiddleCenter;
+            lblVez.UseCompatibleTextRendering = true;
             // 
             // pnlTelaInicial
             // 
+            pnlTelaInicial.BackColor = Color.Transparent;
+            pnlTelaInicial.BackgroundImage = Properties.Resources.fundo;
             pnlTelaInicial.Controls.Add(btnSair);
             pnlTelaInicial.Controls.Add(btnJogar);
             pnlTelaInicial.Controls.Add(lblTitulo);
             pnlTelaInicial.Dock = DockStyle.Fill;
             pnlTelaInicial.Location = new Point(0, 0);
+            pnlTelaInicial.Margin = new Padding(5);
             pnlTelaInicial.Name = "pnlTelaInicial";
-            pnlTelaInicial.Size = new Size(284, 352);
+            pnlTelaInicial.Size = new Size(458, 592);
             pnlTelaInicial.TabIndex = 14;
+            // 
+            // pnlTelaFinal
+            // 
+            pnlTelaFinal.BackgroundImage = Properties.Resources.fundo;
+            pnlTelaFinal.Controls.Add(button2);
+            pnlTelaFinal.Controls.Add(button1);
+            pnlTelaFinal.Controls.Add(lblReceba);
+            pnlTelaFinal.Controls.Add(lblTituloFinal);
+            pnlTelaFinal.Dock = DockStyle.Fill;
+            pnlTelaFinal.Location = new Point(0, 0);
+            pnlTelaFinal.Name = "pnlTelaFinal";
+            pnlTelaFinal.Size = new Size(458, 592);
+            pnlTelaFinal.TabIndex = 3;
             // 
             // btnSair
             // 
-            btnSair.BackColor = Color.DimGray;
+            btnSair.BackColor = Color.FromArgb(64, 128, 128, 128);
             btnSair.FlatAppearance.BorderSize = 0;
+            btnSair.FlatAppearance.MouseOverBackColor = Color.FromArgb(128, 128, 128, 128);
             btnSair.FlatStyle = FlatStyle.Flat;
             btnSair.Font = new Font("MV Boli", 12F, FontStyle.Bold);
             btnSair.ForeColor = Color.WhiteSmoke;
-            btnSair.Location = new Point(96, 190);
+            btnSair.Location = new Point(153, 320);
+            btnSair.Margin = new Padding(5);
             btnSair.Name = "btnSair";
-            btnSair.Size = new Size(95, 35);
+            btnSair.Size = new Size(158, 59);
             btnSair.TabIndex = 2;
             btnSair.Text = "Sair";
             btnSair.UseVisualStyleBackColor = false;
@@ -305,14 +345,16 @@
             // 
             // btnJogar
             // 
-            btnJogar.BackColor = Color.DimGray;
+            btnJogar.BackColor = Color.FromArgb(64, 128, 128, 128);
             btnJogar.FlatAppearance.BorderSize = 0;
+            btnJogar.FlatAppearance.MouseOverBackColor = Color.FromArgb(128, 128, 128, 128);
             btnJogar.FlatStyle = FlatStyle.Flat;
             btnJogar.Font = new Font("MV Boli", 12F, FontStyle.Bold);
             btnJogar.ForeColor = Color.WhiteSmoke;
-            btnJogar.Location = new Point(96, 149);
+            btnJogar.Location = new Point(153, 251);
+            btnJogar.Margin = new Padding(5);
             btnJogar.Name = "btnJogar";
-            btnJogar.Size = new Size(95, 35);
+            btnJogar.Size = new Size(158, 59);
             btnJogar.TabIndex = 1;
             btnJogar.Text = "Jogar";
             btnJogar.UseVisualStyleBackColor = false;
@@ -322,27 +364,73 @@
             // 
             lblTitulo.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             lblTitulo.AutoSize = true;
+            lblTitulo.BackColor = Color.Transparent;
             lblTitulo.FlatStyle = FlatStyle.Flat;
-            lblTitulo.Font = new Font("Segoe Script", 25F, FontStyle.Bold);
-            lblTitulo.Location = new Point(4, 23);
+            lblTitulo.Font = new Font("Segoe Script", 26F, FontStyle.Bold);
+            lblTitulo.ForeColor = Color.WhiteSmoke;
+            lblTitulo.Location = new Point(17, 79);
+            lblTitulo.Margin = new Padding(5, 0, 5, 0);
             lblTitulo.Name = "lblTitulo";
-            lblTitulo.Size = new Size(277, 57);
+            lblTitulo.Size = new Size(419, 87);
             lblTitulo.TabIndex = 0;
             lblTitulo.Text = "Jogo da Velha";
             lblTitulo.TextAlign = ContentAlignment.MiddleCenter;
             // 
+            // lblTituloFinal
+            // 
+            lblTituloFinal.AutoSize = true;
+            lblTituloFinal.BackColor = Color.Transparent;
+            lblTituloFinal.Font = new Font("Segoe Script", 20F);
+            lblTituloFinal.Location = new Point(91, 62);
+            lblTituloFinal.Name = "lblTituloFinal";
+            lblTituloFinal.Size = new Size(295, 64);
+            lblTituloFinal.TabIndex = 0;
+            lblTituloFinal.Text = "LoremIpsum";
+            // 
+            // lblReceba
+            // 
+            lblReceba.AutoSize = true;
+            lblReceba.BackColor = Color.Transparent;
+            lblReceba.Font = new Font("Segoe Script", 20F);
+            lblReceba.Location = new Point(127, 139);
+            lblReceba.Name = "lblReceba";
+            lblReceba.Size = new Size(211, 64);
+            lblReceba.TabIndex = 1;
+            lblReceba.Text = "RECEBA";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(158, 264);
+            button1.Name = "button1";
+            button1.Size = new Size(153, 48);
+            button1.TabIndex = 2;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(160, 331);
+            button2.Name = "button2";
+            button2.Size = new Size(153, 48);
+            button2.TabIndex = 3;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
             // frmJogoDaVelha
             // 
-            AutoScaleDimensions = new SizeF(6F, 14F);
+            AutoScaleDimensions = new SizeF(15F, 37F);
             AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
             BackColor = Color.FromArgb(20, 20, 20);
-            ClientSize = new Size(284, 352);
-            Controls.Add(label1);
+            BackgroundImage = Properties.Resources.fundo;
+            ClientSize = new Size(458, 592);
+            Controls.Add(pnlTelaFinal);
+            Controls.Add(lblVez);
             Controls.Add(panel4);
             Controls.Add(panel3);
             Controls.Add(panel2);
             Controls.Add(panel1);
-            Controls.Add(button1);
+            Controls.Add(btnReiniciar);
             Controls.Add(btn9);
             Controls.Add(btn8);
             Controls.Add(btn7);
@@ -353,16 +441,18 @@
             Controls.Add(btn2);
             Controls.Add(btn1);
             Controls.Add(pnlTelaInicial);
-            Font = new Font("Calibri", 9F);
-            ForeColor = Color.White;
+            Font = new Font("Calibri", 15F);
+            ForeColor = Color.Silver;
             FormBorderStyle = FormBorderStyle.FixedSingle;
+            Margin = new Padding(5);
             MaximizeBox = false;
             Name = "frmJogoDaVelha";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Jogo Da Velha";
-            Click += BotaoGrid_Click;
             pnlTelaInicial.ResumeLayout(false);
             pnlTelaInicial.PerformLayout();
+            pnlTelaFinal.ResumeLayout(false);
+            pnlTelaFinal.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -377,16 +467,21 @@
         private Button btn9;
         private Button btn8;
         private Button btn7;
-        private Button button1;
+        private Button btnReiniciar;
         private Panel panel1;
         private Panel panel2;
         private Panel panel3;
         private Panel panel4;
-        private Label label1;
+        private Label lblVez;
         private Panel pnlTelaInicial;
         private Label lblTitulo;
         private Button btnJogar;
         private FontDialog fontDialog1;
         private Button btnSair;
+        private Panel pnlTelaFinal;
+        private Button button1;
+        private Label lblReceba;
+        private Label lblTituloFinal;
+        private Button button2;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,18 +1,71 @@
 using System.DirectoryServices.ActiveDirectory;
+using System.Media;
 
 namespace JogoDaVelha
 {
     public partial class frmJogoDaVelha : Form
     {
+        private bool _vezDoX = true;
+        private int _jogadas = 0;
+        private SoundPlayer _somClique = new SoundPlayer("escrita.wav");
+
         public frmJogoDaVelha()
         {
             InitializeComponent();
             pnlTelaInicial.BringToFront();
+            _somClique.Load();
         }
 
         private void BotaoGrid_Click(object sender, EventArgs e)
         {
+            Button botaoClicado = (Button)sender;
 
+            if (botaoClicado.Text != "") return;
+            botaoClicado.Text = _vezDoX ? "X" : "O";
+            _jogadas++;
+            _somClique.Play();
+
+            if (VerificarVencedor())
+            {
+                string vencedor = _vezDoX ? "X" : "O";
+                MessageBox.Show($"Jogador {vencedor} venceu!", "Temos um Vencedor", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                ZerarTabuleiro();
+                pnlTelaInicial.Visible = true;
+
+            }
+            else if (_jogadas == 9)
+            {
+                MessageBox.Show("Deu velha! O jogo empatou", "Empate", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                ZerarTabuleiro();
+                pnlTelaInicial.Visible = true;
+            }
+            else
+            {
+                _vezDoX = !_vezDoX;
+                lblVez.Text = $"Vez do jogador: {(_vezDoX ? "X" : "O")}";
+            }
+            
+        }
+
+        private bool VerificarVencedor()
+        {
+            if (ChecarTrinca(btn1, btn2, btn3)) return true;
+            if (ChecarTrinca(btn4, btn5, btn6)) return true;
+            if (ChecarTrinca(btn7, btn8, btn9)) return true;
+
+            if (ChecarTrinca(btn1, btn4, btn7)) return true;
+            if (ChecarTrinca(btn2, btn5, btn8)) return true;
+            if (ChecarTrinca(btn3, btn6, btn9)) return true;
+
+            if (ChecarTrinca(btn1, btn5, btn9)) return true;
+            if (ChecarTrinca(btn3, btn5, btn7)) return true;
+            return false;
+        }
+
+        private bool ChecarTrinca(Button b1, Button b2, Button b3)
+        {
+            if (b1.Text == "") return false;
+            return (b1.Text == b2.Text && b2.Text == b3.Text);
         }
 
         private void btnSair_Click(object sender, EventArgs e)
@@ -23,6 +76,21 @@ namespace JogoDaVelha
         private void btnJogar_Click(object sender, EventArgs e)
         {
             pnlTelaInicial.Visible = false;
+        }
+
+        private void btnReiniciar_Click(object sender, EventArgs e)
+        {
+            ZerarTabuleiro();
+        }
+
+        private void ZerarTabuleiro()
+        {
+            btn1.Text = ""; btn2.Text = ""; btn3.Text = "";
+            btn4.Text = ""; btn5.Text = ""; btn6.Text = "";
+            btn7.Text = ""; btn8.Text = ""; btn9.Text = "";
+            _vezDoX = true;
+            _jogadas = 0;
+            lblVez.Text = "Vez do jogador: X";
         }
     }
 }
